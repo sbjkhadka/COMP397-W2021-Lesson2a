7e139a9720cbec1b96efa97e74e740d7ae0d43d6
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-01-26 07:25:39 -0500

    Added 2nd tile to the maze and I completed my lab task with this.

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivity = 1000;
+    public float mouseSensitivity = 1000.0f;
     public Transform playerBody;
 
     private float XRotation = 0.0f;
@@ -24,6 +24,7 @@ public class CameraController : MonoBehaviour
 
         XRotation -= mouseY;
         XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
+
         transform.localRotation = Quaternion.Euler(XRotation, 0.0f, 0.0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
